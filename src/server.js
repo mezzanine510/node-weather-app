e6b5afd05bad449a497004d1d5d7dd6536d26c2e
mezzanine510 <mezzanine510@gmail.com>
2019-05-13 23:41:07 -0700

    site sections set up; templating set up using hbs(handlebars); links are functional; images/icons added

---
+++ src/server.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const chalk = require('chalk');
+
+const port = process.env.PORT = 3000;
+
+const app = express();
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine','hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use( express.static(publicDirectoryPath) );
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Nahuel Ruda'
+    });
+});
+
+// About
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Nahuel Ruda'
+    });
+});
+
+// Help
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        details: 'This is the help page.',
+        name: 'Poopy Diaper'
+    });
+});
+
+// Weather
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'Please enter an address'
+        });
+    }
+    
+    res.send({
+        forecast: 'Sunny as fuck',
+        address: req.query.address
+    });
+});
+
+// Products
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        });
+    }
+    
+    console.log(req.query.search);
+    res.send({ products: [] });
+});
+
+// 404 related to /help
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Nahuel Ruda',
+        errorMessage: 'Help article was not found.'
+    });
+});
+
+// 404
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Nahuel Ruda',
+        errorMessage: 'Page was not found.'
+    });
+});
+
+app.listen(port, () => {
+    console.log( chalk.cyan('Server is up on port'), chalk.yellow(port) );
+});
